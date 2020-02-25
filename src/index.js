e444d270e74af6a85e6c6dd593ff825d92dbb953
katrin-kot <enkor89@mail.ru>
2020-02-25 20:25:42 +0300

    feat: all tests are passing

--- src/index.js
+++ src/index.js
@@ -1,3 +1,67 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    const read = [
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+        "",
+        "ten",
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+        "hundred"
+    ];
+    function ten(number) {
+        if (number % 10 === 0 && number / 10 < 10) {
+            return read[20 + number / 10];
+        }
+        if (number % 10 != 0 && number / 10 < 10) {
+            return read[20 + Math.floor(number / 10)] + " " + read[number % 10];
+        }
+    }
+    if (number >= 0 && number <= 19 && number != 10) {
+        return read[number];
+    }
+    if (number % 10 === 0 && number / 10 < 10) {
+        return read[20 + number / 10];
+    }
+    if (number % 10 != 0 && number / 10 < 10) {
+        return read[20 + Math.floor(number / 10)] + " " + read[number % 10];
+    }
+
+    if (number % 100 === 0 && number / 100 < 10) {
+        return read[number / 100] + " " + read[30];
+    }
+
+    if (number % 100 != 0 && number / 100 < 10) {
+        let hundred = read[Math.floor(number / 100)] + " " + read[30];
+        if (number % 100 > 19) {
+            return hundred + " " + ten(number % 100);
+        } else if (number % 100 === 10) {
+            return hundred + " " + read[21];
+        } else {
+            return hundred + " " + read[number % 100];
+        }
+    }
+};
